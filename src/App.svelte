755062bf3129792acde8957b431c6d64f5099c95
mugendi <mugendinjue7@gmail.com>
2023-01-23 08:10:56 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<!-- 
+
+  Importing the global status here enables utilization of HMR (Hot Module Replacement)[Hot reload].
+  This can also be done at the index.html file too.
+ -->
+<script lang="ts">
+  import '$root/styles/global.css'
+  import Todos from './components/Todos.svelte';
+</script>
+
+<Todos/>
---
+++ src/types/todo.ts
@@ -0,0 +1,5 @@
+export interface Itodo {
+    id: string,
+    text: string,
+    completed: boolean
+}
